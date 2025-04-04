ed191e838c0ddf7b4db65ab9225d029b8b9aa2c9
David <lehoangduy26112000@gmail.com>
2025-04-03 20:45:34 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(
-    "mongodb+srv:**********@cluster0.0aomf7p.mongodb.net/unicornsdb?retryWrites=true&w=majority"
+    "mongodb+srv:**********@cluster0.0aomf7p.mongodb.net/unicornsdb?retryWrites=true&w=majority" //we can use .env
   )
   .then(() => console.log(" MongoDB connected"))
   .catch((err) => console.error(" MongoDB connection error:", err));
